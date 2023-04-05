b93dad25d364d8e5b6069f0517796a048b6a2361
MatiasEchavarria <matiasechavarria34@hotmail.com>
2023-04-05 09:59:54 -0300

    agregamos un bottomnavbar

--- lib/UI/home_layout/mobile/home_screen.dart
+++ lib/UI/home_layout/mobile/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:landing_page/UI/home_layout/mobile/screens/first_section_screen.dart';
 import 'package:provider/provider.dart';
 
 import '../../../theme/theme_changer.dart';
+import '../../../utils/utils.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -13,23 +15,55 @@ class HomeScreen extends StatelessWidget {
     final w = MediaQuery.of(context).size.width;
     final appTheme = Provider.of<ThemeCharger>(context).currentTheme;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Shelby Barber',
-          style: TextStyle(
-            color: appTheme.colorScheme.primary,
+        appBar: AppBar(
+          title: Text(
+            'Shelby Barber',
+            style: TextStyle(
+              color: appTheme.colorScheme.primary,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.black,
+        ),
+        body: SizedBox(
+          height: h,
+          width: w,
+          child: Column(
+            children: const [FirstSectionScreen()],
           ),
         ),
-        centerTitle: true,
-        backgroundColor: Colors.black,
-      ),
-      body: SizedBox(
-        height: h,
-        width: w,
-        child: Column(
-          children: const [FirstSectionScreen()],
-        ),
-      ),
-    );
+        bottomNavigationBar: SingleChildScrollView(
+          physics: const NeverScrollableScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          child: GNav(
+            gap: 15,
+            activeColor: Colors.amber,
+            color: Colors.white,
+            hoverColor: Colors.red,
+            mainAxisAlignment: MainAxisAlignment.center,
+            rippleColor: Colors.blue,
+            tabBackgroundColor: Colors.white12,
+            textStyle: FontsApp.oldStandardTt,
+            tabs: [
+              GButton(
+                margin: EdgeInsets.all(SizesApp.padding10),
+                icon: Icons.access_alarm,
+                text: 'Turnos',
+              ),
+              GButton(
+                icon: Icons.chat,
+                text: 'Chat',
+              ),
+              GButton(
+                icon: Icons.notification_add,
+                text: 'Avisos',
+              ),
+              GButton(
+                icon: Icons.person_off_outlined,
+                text: 'Perfil',
+              ),
+            ],
+          ),
+        ));
   }
 }
--- lib/UI/home_layout/web/homeview.dart
+++ lib/UI/home_layout/web/homeview.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-//dependencies
 
-//file addresses
-import 'home_view_body/0.header/header_layout.dart';
 import 'home_view_body/home_view_body.dart';
+import 'home_view_body/0.header/header_layout.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
+
   @override
   State<HomeView> createState() => _HomeViewState();
 }
@@ -16,7 +15,9 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
+
         children: const [
+          
           HomeViewBody(),
           Headerlayout(),
           Positioned(
